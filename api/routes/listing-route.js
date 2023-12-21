904e71c00eae26c4cf2e7c2fffdfc05ef003f427
Manish Tomer <mt0578902@gmail.com>
2023-12-21 14:43:55 +0530

    created search api route

--- api/routes/listing-route.js
+++ api/routes/listing-route.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing-controller.js";
 import { varifyToken } from "../utils/verify-user.js";
 const router = Router();
@@ -11,4 +12,5 @@ router.post("/create", varifyToken, createListing);
 router.delete("/delete/:id", varifyToken, deleteListing);
 router.post("/update/:id", varifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 export default router;
