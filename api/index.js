9d63ad99d1d14f7f61bd556a2d9c81ea593af7ca
Manish Tomer <mt0578902@gmail.com>
2023-12-29 08:48:51 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import connect from "./config/db-config.js";
-
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 const app = express();
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api", router);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
